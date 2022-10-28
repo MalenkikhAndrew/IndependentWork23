fc3298222685933093e57c706d0379442662bc70
User <User@DESKTOP-A50L8A1>
2022-10-28 16:15:50 +1000

    Добавьте файлы проекта.

---
+++ IndependentWork23/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace IndependentWork23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            Ftorial(n);
+            Console.ReadKey();
+        }
+        static void Ftorial(int n) 
+        {
+            
+            int fto=1;
+            for (int i = 2; i <= n; i++) 
+            {
+                fto = fto * i;
+                Thread.Sleep(500);
+            }
+            Console.WriteLine(fto);
+        }
+        static async void FtorialAsunc(int n) 
+        {
+            await Task.Run(() => Ftorial(n));
+        }
+    }
+}
